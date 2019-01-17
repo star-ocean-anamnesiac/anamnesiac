ce7613a1bd86d5bda5407040986b6fb4564cbc0c
Kyle J. Kemp <kyle@seiyria.com>
2019-01-17 14:01:22 -0600

    Update character-list.page.ts

--- src/app/character-list/character-list.page.ts
+++ src/app/character-list/character-list.page.ts
@@ -217,9 +217,9 @@ export class CharacterListPage implements OnInit, OnDestroy {
       .sortBy([(char) => -Math.floor(char.rating), 'name'])
       .groupBy(char => {
         if(char.rating >= 10) { return 'Top Tier (10/10)'; }
-        if(char.rating >= 8 && char.rating <= 9) { return 'Great (8-9/10)'; }
-        if(char.rating >= 6 && char.rating <= 7) { return 'Good (6-7/10)'; }
-        if(char.rating >= 5 && char.rating <= 4) { return 'Average (4-5/10)'; }
+        if(char.rating >= 8 && char.rating <  9) { return 'Great (8-9/10)'; }
+        if(char.rating >= 6 && char.rating <  7) { return 'Good (6-7/10)'; }
+        if(char.rating >= 4 && char.rating <  6) { return 'Average (4-5/10)'; }
         return 'Bad (1-3/10)';
       })
       .value();
