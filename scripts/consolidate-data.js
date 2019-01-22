2a8d688e39f15da665cd1a71a78fa35702892cd8
seiyria <kyle@seiyria.com>
2019-01-21 20:29:53 -0600

    stop using fs promises api

--- scripts/consolidate-data.js
+++ scripts/consolidate-data.js
@@ -2,57 +2,54 @@
 const _ = require('lodash');
 
 const YAML = require('js-yaml');
-const { promises } = require('fs');
+const fs = require('fs');
 
 const ROOT_FILE = 'src/assets/data/root.yml';
 const CHANGELOG_FILE = 'src/assets/data/changelog.yml';
 
-const run = async () => {
-  const data = await promises.readFile(ROOT_FILE, 'utf-8');
-  const changelogData = await promises.readFile(CHANGELOG_FILE, 'utf-8');
-  
-  const root = YAML.safeLoad(data);
-  const changelog = YAML.safeLoad(changelogData);
+const data = fs.readFileSync(ROOT_FILE, 'utf-8');
+const changelogData = fs.readFileSync(CHANGELOG_FILE, 'utf-8');
 
-  const { classes, weapons, accessories } = root;
-  
-  const allItems = _.flattenDeep(await Promise.all(weapons.concat(accessories).map(async ({ id }) => {
-    const path = id === 'all' ? `accessory/all` : `weapon/${id}`;
-    const data = await promises.readFile(`src/assets/data/item/${path}.yml`, 'utf-8');
-    const items = YAML.safeLoad(data);
+const root = YAML.safeLoad(data);
+const changelog = YAML.safeLoad(changelogData);
 
-    if(id === 'all') {
-      accessories.forEach(weap => {
-        weap.type = 'accessory';
-        weap.subtype = id;
-      });
-    } else {
-      items.forEach(item => {
-        item.type = 'weapon';
-        item.subtype = id;
-      });
-    }
+const { classes, weapons, accessories } = root;
 
-    return items;
-  })));
+const allItems = _.flattenDeep(weapons.concat(accessories).map(({ id }) => {
+  const path = id === 'all' ? `accessory/all` : `weapon/${id}`;
+  const data = fs.readFileSync(`src/assets/data/item/${path}.yml`, 'utf-8');
+  const items = YAML.safeLoad(data);
 
-  const allCharacters = _.flattenDeep(await Promise.all(classes.map(async charClass => {
-    const data = await promises.readFile(`src/assets/data/character/${charClass.toLowerCase()}.yml`, 'utf-8');
-    const characters = YAML.safeLoad(data);
+  if(id === 'all') {
+    accessories.forEach(weap => {
+      weap.type = 'accessory';
+      weap.subtype = id;
+    });
+  } else {
+    items.forEach(item => {
+      item.type = 'weapon';
+      item.subtype = id;
+    });
+  }
 
-    characters.forEach(char => char.type = charClass.toLowerCase());
+  return items;
+}));
 
-    return characters;
-  })));
+const allCharacters = _.flattenDeep(classes.map(charClass => {
+  const data = fs.readFileSync(`src/assets/data/character/${charClass.toLowerCase()}.yml`, 'utf-8');
+  const characters = YAML.safeLoad(data);
 
-  const fullData = {
-    root,
-    changelog,
-    allCharacters,
-    allItems
-  };
+  characters.forEach(char => char.type = charClass.toLowerCase());
 
-  await promises.writeFile('src/app/data.json', JSON.stringify(fullData));
+  return characters;
+}));
+
+const fullData = {
+  root,
+  changelog,
+  allCharacters,
+  allItems
 };
 
-run();
+fs.writeFileSync('src/app/data.json', JSON.stringify(fullData));
+
