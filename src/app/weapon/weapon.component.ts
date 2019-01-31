3703bf12031ee674839e8eff7f9b8668df283d44
seiyria <kyle@seiyria.com>
2019-01-31 12:17:34 -0600

    auto import is drunk

--- src/app/weapon/weapon.component.ts
+++ src/app/weapon/weapon.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { DataService } from '../data.service';
-import { ChangeDetectionStrategy } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-weapon',
