406bcdf00eb5d3ad4e43847585becb592fcce48a
seiyria <kyle@seiyria.com>
2019-02-01 10:46:59 -0600

    adjust item icons

--- scripts/pngcrush.js
+++ scripts/pngcrush.js
@@ -5,7 +5,7 @@ const pngquant = require('imagemin-pngquant');
 imagemin([
   'src/assets/**/*-icons.png'
 ], 'src/assets/spritesheets', {
-  plugins: [pngquant({ quality: [0.1, 0.3] })]
+  plugins: [pngquant({ quality: [0.1, 0.2] })]
 }).then(() => {
   console.log('PNGCrush done.');
 });
