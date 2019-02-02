4b7c3541c2cf100d8e2266ae646905e81d505e42
seiyria <kyle@seiyria.com>
2019-02-02 17:33:10 -0600

    add test to ensure valid metadata

--- src/test/validate-meta.ts
+++ src/test/validate-meta.ts
@@ -1,12 +1,55 @@
 
+import { includes } from 'lodash';
+
+const validMetas = [
+  'ATK', 'INT', 'DEF', 'GRD', 'HIT', 'HP',
+
+  'AP Recovery', 'AP Recovery From Normal Attacks', 'AP Cost Reduction',
+
+  'Elemental Damage', 'Fire Damage', 'Ice Damage', 'Light Damage', 
+  'Dark Damage', 'Earth Damage', 'Lightning Damage', 'Wind Damage',
+
+  'Fire Damage Reduction', 'Ice Damage Reduction', 'Light Damage Reduction', 'Dark Damage Reduction',
+  'Earth Damage Reduction', 'Lightning Damage Reduction', 'Wind Damage Reduction',
+
+  'Crit Rate', 'Crit Damage',
+
+  'Damage To Humans', 'Damage To Beasts', 'Damage To Machines', 'Damage To Demons', 
+  'Damage To Divinities', 'Damage To Plants', 'Damage To Birds', 'Damage To Dragons',
+  'Damage To Insects', 'Damage To Undead',
+
+  'Stun Rate',
+
+  'Damage', 'INT Damage', 'ATK Damage',
+  'Damage Taken Reduction', 'INT Damage Taken Reduction', 'ATK Damage Taken Reduction',
+
+  'Rush Damage',
+
+  'Single Target Damage', 'Multiple Target Damage',
+
+  'Silence Attack Chance', 'Poison Attack Chance', 'Curse Attack Chance', 
+  'Paralysis Attack Chance', 'Freeze Attack Chance',
+
+  'Cast Time Reduction',
+
+  'DEF Reduction', 'ATK Reduction', 'INT Reduction', 'HIT Reduction', 'GRD Reduction',
+  'Enemy DEF Reduction', 'Enemy ATK Reduction', 'Enemy INT Reduction', 'Enemy HIT Reduction', 'Enemy GRD Reduction'
+];
+
 export const validateMeta = (t, meta, parenName) => {
   if(!meta) return;
 
-  t.truthy(meta.buff || meta.buffs, 'meta must have a buff or buffs' + parenName);
   if(meta.buffs) {
     t.true(meta.buffs.length > 0, 'meta buffs must exist' + parenName);
+    meta.buffs.forEach(buff => {
+      t.true(includes(validMetas, buff), 'meta (' + buff + ') must be a valid buff ' + parenName);
+    });
+  } else {
+    t.truthy(meta.buff, 'meta (' + meta.buff + ') must have a buff or buffs' + parenName);
+    t.true(includes(validMetas, meta.buff), 'meta (' + meta.buff + ') must be a valid buff' + parenName);
   }
-  t.true(meta.buffValue > 0, 'meta must have a value > 0' + parenName);
+  
+  t.true(meta.buffValue > 0, 'meta  must have a value > 0' + parenName);
   t.true(meta.priority > 0 && meta.priority < 5, 'meta must have a priority 1..4' + parenName);
 
   t.falsy(meta.source, 'meta source should not be set' + parenName);
