7f144ae0cb3962cf2f630b0b92d24c2acf22ec23
Kyle Kemp <kyle@seiyria.com>
2019-02-23 08:24:35 -0600

    probably not working for this reason

--- scripts/build-api.js
+++ scripts/build-api.js
@@ -1,6 +1,7 @@
 
 const fs = require('fs');
 
+fs.copyFileSync('src/app/data.json', 'www/data.json');
 fs.writeFileSync('www/_headers', `/*\r\n\tX-Commit: ${process.env.COMMIT_REF}`);
 
 console.log('Headers built.');
--- scripts/build-cleanup.js
+++ scripts/build-cleanup.js
@@ -1,5 +1,4 @@
 
-const fs = require('fs');
 const rimraf = require('rimraf');
 
 rimraf.sync('www/characters');
@@ -8,7 +7,5 @@ rimraf.sync('www/assets/data');
 
 rimraf.sync('www/assets/icons/**/*.png');
 rimraf.sync('www/assets/items/**/*.png');
-
-fs.copyFileSync('src/app/data.json', 'www/data.json');
 
 console.log('Cleanup done.');
