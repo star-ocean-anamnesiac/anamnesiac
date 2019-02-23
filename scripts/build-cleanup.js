1bcb282b2dac6d5b873b51b24f33e543e18f32e3
Kyle Kemp <kyle@seiyria.com>
2019-02-23 07:50:35 -0600

    expose data file for others to parse

--- scripts/build-cleanup.js
+++ scripts/build-cleanup.js
@@ -1,4 +1,5 @@
 
+const fs = require('fs');
 const rimraf = require('rimraf');
 
 rimraf.sync('www/characters');
@@ -7,3 +8,5 @@ rimraf.sync('www/assets/data');
 
 rimraf.sync('www/assets/icons/**/*.png');
 rimraf.sync('www/assets/items/**/*.png');
+
+fs.copyFileSync('src/app/data.json', 'www/assets/data.json');
