597a6f5f10a27986e53caff8bb5116fc1d95c077
Kyle Kemp <kyle@seiyria.com>
2019-02-23 08:21:16 -0600

    confirmation

--- scripts/build-headers.js
+++ scripts/build-headers.js
@@ -1,4 +1,6 @@
 
 const fs = require('fs');
 
-fs.writeFileSync('www/_headers', `/*\r\n\tX-Commit: ${process.env.COMMIT_REF}`)
+fs.writeFileSync('www/_headers', `/*\r\n\tX-Commit: ${process.env.COMMIT_REF}`);
+
+console.log('Headers built.');
