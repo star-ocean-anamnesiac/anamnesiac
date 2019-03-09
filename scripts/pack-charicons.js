1bdfddc8ad40975703fa85284d414156ad4febdb
seiyria <kyle@seiyria.com>
2019-03-08 18:18:47 -0600

    add new images, new format, etc

--- scripts/pack-charicons.js
+++ scripts/pack-charicons.js
@@ -1,8 +1,31 @@
 
 const packer = require('spritesheet-js');
+const rimraf = require('rimraf');
 
-packer('src/assets/characters/**/!(\+)*.png', { format: 'json', name: '+char-icons', path: 'src/assets/characters' }, (err) => {
-  if(!err) return;
-  console.error(err);
-  process.exit(1);
-});
+// clean directory in case it needs it
+rimraf.sync('src/assets/characters/\+*');
+
+exec('gm mogrify -format "formatted.png" -trim -resize 128x128! "src/assets/characters/*.png"');
+
+packer(
+  'src/assets/characters/**/*.formatted.png', 
+
+  { format: 'json', name: '+char-icons', path: 'src/assets/characters' }, 
+  
+  (err) => {
+    if(!err) {
+
+      // rewrite atlas file
+      fs.writeFileSync(
+        'src/assets/character/+char-icons.json',
+        fs.readFileSync('src/assets/characters/+char-icons.json', 'utf-8').split('.formatted').join('')
+      );
+
+      // remove formatted images
+      rimraf.sync('src/assets/characters/**/*.formatted.png');
+      return;
+    }
+
+    console.error(err);
+    process.exit(1);
+  });
