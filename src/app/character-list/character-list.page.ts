511e78a7039bb4a287d100b32ffb5000a4c02076
Kyle J. Kemp <kyle@seiyria.com>
2019-05-16 09:04:48 -0500

    Revert "potential virtual scroll rewrite - revert if unable to sort issues"
    This reverts commit b62537d6f47d99d755655ab3b82ee916773dea54.

--- src/app/character-list/character-list.page.ts
+++ src/app/character-list/character-list.page.ts
@@ -43,13 +43,11 @@ export class CharacterListPage implements OnInit, OnDestroy {
   public gridView: boolean;
 
   public tierSortedCharacters: { [key: string]: Character[] } = {};
-  public tierOrderedCharacters: Character[] = [];
   public allTiers: string[] = [];
 
   public alphaSortedCharacters: Character[] = [];
 
   public weaponSortedCharacters: { [key: string]: Character[] } = {};
-  public weaponOrderedCharacters: Character[] = [];
   public allWeapons: string[] = [];
 
   public showSearch: boolean;
@@ -120,20 +118,11 @@ export class CharacterListPage implements OnInit, OnDestroy {
         this.tierSortedCharacters = res.tierSortedCharacters;
         this.weaponSortedCharacters = res.weaponSortedCharacters;
 
-        this.tierOrderedCharacters = _.flatten(Object.values(this.tierSortedCharacters));
-        this.weaponOrderedCharacters = _.flatten(Object.values(this.weaponSortedCharacters));
-
         if(this.getPreviouslyLoadedChar()) {
           this.loadCharacterModal(this.getPreviouslyLoadedChar());
         }
       });
     });
-  }
-
-  public characterHeader = (char: Character) => {
-    if(this.sorting === 'tier')   { return { text: this.getTierNameForNumber(char.rating) }; }
-    if(this.sorting === 'weapon') { return { weapon: char.weapon }; }
-    return null;
   }
 
   private updateRegionBasedOn(val: boolean) {
@@ -263,17 +252,6 @@ export class CharacterListPage implements OnInit, OnDestroy {
     this.searchValue = '';
   }
 
-  private getTierNameForNumber(tier: number): string {
-    if(tier > 10)              { return 'Meta-defining'; }
-    if(tier === 10)            { return 'Best of the best'; }
-    if(tier >= 8 && tier < 10) { return 'Preferred in most situations'; }
-    if(tier >= 6 && tier <  8) { return 'Preferred in specific situations'; }
-    if(tier >= 4 && tier <  6) { return 'Good in most situations'; }
-    if(tier >= 2 && tier <  4) { return 'Good in specific situations'; }
-    if(tier >= 0 && tier <  2) { return 'Outclassed'; }
-    return 'Unranked';
-  }
-
   // CHARACTER LIST SORTING
   private getCharacterUpdateList() {
     let arr = this.allCharacters;
@@ -334,7 +312,14 @@ export class CharacterListPage implements OnInit, OnDestroy {
       tierSortedCharacters = _(arr)
         .sortBy([(char) => -Math.floor(char.rating), 'name'])
         .groupBy(char => {
-          return this.getTierNameForNumber(char.rating);
+          if(char.rating > 10)                     { return 'Meta-defining'; }
+          if(char.rating === 10)                   { return 'Best of the best'; }
+          if(char.rating >= 8 && char.rating < 10) { return 'Preferred in most situations'; }
+          if(char.rating >= 6 && char.rating <  8) { return 'Preferred in specific situations'; }
+          if(char.rating >= 4 && char.rating <  6) { return 'Good in most situations'; }
+          if(char.rating >= 2 && char.rating <  4) { return 'Good in specific situations'; }
+          if(char.rating >= 0 && char.rating <  2) { return 'Outclassed'; }
+          return 'Unranked';
         })
         .value();
 
